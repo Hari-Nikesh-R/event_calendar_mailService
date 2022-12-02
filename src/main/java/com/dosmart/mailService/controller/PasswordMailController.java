5089aaa06578a94cc46970e062824ba068a42bbd
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2022-12-02 14:01:14 +0530

    Initial commit

---
+++ src/main/java/com/dosmart/mailService/controller/PasswordMailController.java
@@ -0,0 +1,13 @@
+package com.dosmart.mailService.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/pass-code")
+public class PasswordMailController {
+
+
+}
